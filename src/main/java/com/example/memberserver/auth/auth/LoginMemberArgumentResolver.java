5d8a1639a1a08d1f423a05ce2674448deb7d9d72
Dsys1129 <dsys1129@gmail.com>
2023-08-21 18:24:00 +0900

    feat: #1 ArgumentResolver 추가

---
+++ src/main/java/com/example/memberserver/auth/auth/LoginMemberArgumentResolver.java
@@ -0,0 +1,31 @@
+package com.example.memberserver.auth.auth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.util.Enumeration;
+
+@Slf4j
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasLoginMemberAnnotation = parameter.hasParameterAnnotation(LoginMember.class);
+        boolean isSupportType = Long.class.isAssignableFrom(parameter.getParameterType());
+        return hasLoginMemberAnnotation && isSupportType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+        NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
+        return Long.parseLong(request.getHeader("x-authorization-id"));
+    }
+}
---
+++ src/main/java/com/example/memberserver/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.memberserver.config;
+
+import com.example.memberserver.auth.auth.LoginMemberArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
+}
