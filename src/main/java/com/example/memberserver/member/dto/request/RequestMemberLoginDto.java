215ce37825b00c9e6892e0fe1067cb3f43be47e9
Dsys1129 <dsys1129@gmail.com>
2023-09-08 14:29:44 +0900

    refactor: 쓰이지 않는 코드 정리

--- src/main/java/com/example/memberserver/member/dto/request/RequestMemberLoginDto.java
+++ src/main/java/com/example/memberserver/member/dto/request/RequestMemberLoginDto.java
@@ -1,22 +1,11 @@
 package com.example.memberserver.member.dto.request;
 
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import static lombok.AccessLevel.PROTECTED;
 
 @Getter
-@NoArgsConstructor(access = PROTECTED)
-@Builder
 public class RequestMemberLoginDto {
 
     private String username;
-
     private String password;
 
-    public RequestMemberLoginDto(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
 }
